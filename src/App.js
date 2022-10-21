fea6c2ba0fce93a507b86316103bef7737a8c398
Navait Kumar <navaitk031@gmail.com>
2022-10-20 22:16:23 -0400

    clock component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Clock from "./Clock";
+// import { render } from '@testing-library/react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Navait',
+  lastName: 'Kumar'
+};
+
+const element = (
+<h1>Hello, {formatName(user)}!</h1>
+);
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(element);
+//root.render(<Clock />);
+
+return (
+  <div>
+  <h1>{element}</h1>
+    <Clock/>
+  </div>
+);
 }
 
 export default App;
